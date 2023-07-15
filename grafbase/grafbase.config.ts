8c3b923f66851b3e0377ae1e2b3be5ec4bb02461
Wawan Huang <kurniawanew95@gmail.com>
2023-07-15 19:40:06 +0700

    first commit

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,12 @@
+import { g, auth, config } from "@grafbase/sdk";
+
+const User = g.model("User", {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string(),
+});
+
+export default config({
+  schema: g,
+});
